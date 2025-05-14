d99bf0acfd74f786e80b239c3e6d158a228f53b1
PanibratovR3 <roman.panibratov@gmail.com>
2025-05-14 17:43:29 +0300

    Updated Player object: it contains flag for winning status. Added getter function for Gameboard object. Updated function playRound: it checks winning status of players. Added function in Control object playGame for launching game.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function createPlayer(name, mark) {
-  return { name, mark };
+function createPlayer(name, mark, winStatus) {
+  return { name, mark, winStatus };
 }
 
 const Gameboard = (function () {
@@ -8,72 +8,67 @@ const Gameboard = (function () {
     ["-", "-", "-"],
     ["-", "-", "-"],
   ];
+  const getBoard = () => board;
   const isEmptyCell = (row, column) => board[row][column] === "-";
-  return { board, isEmptyCell };
+  return { getBoard, isEmptyCell };
 })();
 
 const Control = (function () {
   let turnNumber = 1;
   let currentPlayer;
+  let endGame = false;
 
   const playTurn = () => {
     let rowPos;
     let columnPos;
     let incorrectRowPosFlag;
     let incorrectColumnPosFlag;
-
+    let board = Gameboard.getBoard();
     do {
       rowPos = Number(prompt("Enter the number of row (between 0 and 2):"));
       columnPos = Number(
         prompt("Enter the number of column (between 0 and 2):")
       );
       incorrectRowPosFlag =
-        isNaN(rowPos) || rowPos < 0 || rowPos >= Gameboard.board.length;
+        isNaN(rowPos) || rowPos < 0 || rowPos >= board.length;
       incorrectColumnPosFlag =
-        isNaN(columnPos) ||
-        columnPos < 0 ||
-        columnPos >= Gameboard.board.length;
+        isNaN(columnPos) || columnPos < 0 || columnPos >= board.length;
     } while (incorrectRowPosFlag || incorrectColumnPosFlag);
 
     currentPlayer = turnNumber % 2 !== 0 ? playerOne : playerTwo;
     console.log(`${currentPlayer.name}'s move.`);
     if (Gameboard.isEmptyCell(rowPos, columnPos)) {
-      Gameboard.board[rowPos][columnPos] = currentPlayer.mark;
-      console.log(Gameboard.board);
+      board[rowPos][columnPos] = currentPlayer.mark;
+      console.log(board);
+      playerOne.winStatus = checkWinPositions(playerOne.mark);
+      playerTwo.winStatus = checkWinPositions(playerTwo.mark);
+      endGame = playerOne.winStatus || playerTwo.winStatus;
+      turnNumber++;
     }
-    turnNumber++;
+  };
+
+  const playGame = () => {
+    while (!endGame) {
+      playTurn();
+    }
+    if (playerOne.winStatus) {
+      console.log(`${playerOne.name} wins.`);
+    } else if (playerTwo.winStatus) {
+      console.log(`${playerTwo.name} wins`);
+    }
   };
 
   const checkWinPositions = (mark) => {
-    const firstRow = Gameboard.board[0];
-    const secondRow = Gameboard.board[1];
-    const thirdRow = Gameboard.board[2];
+    const board = Gameboard.getBoard();
+    const firstRow = board[0];
+    const secondRow = board[1];
+    const thirdRow = board[2];
 
-    const firstColumn = [
-      Gameboard.board[0][0],
-      Gameboard.board[1][0],
-      Gameboard.board[2][0],
-    ];
-    const secondColumn = [
-      Gameboard.board[0][1],
-      Gameboard.board[1][1],
-      Gameboard.board[2][1],
-    ];
-    const thirdColumn = [
-      Gameboard.board[0][2],
-      Gameboard.board[1][2],
-      Gameboard.board[2][2],
-    ];
-    const firstDiagonal = [
-      Gameboard.board[0][0],
-      Gameboard.board[1][1],
-      Gameboard.board[2][2],
-    ];
-    const secondDiagonal = [
-      Gameboard.board[0][2],
-      Gameboard.board[1][1],
-      Gameboard.board[2][0],
-    ];
+    const firstColumn = [board[0][0], board[1][0], board[2][0]];
+    const secondColumn = [board[0][1], board[1][1], board[2][1]];
+    const thirdColumn = [board[0][2], board[1][2], board[2][2]];
+    const firstDiagonal = [board[0][0], board[1][1], board[2][2]];
+    const secondDiagonal = [board[0][2], board[1][1], board[2][0]];
     const rowFlag =
       firstRow.every((item) => item === mark) ||
       secondRow.every((item) => item === mark) ||
@@ -91,12 +86,13 @@ const Control = (function () {
     return rowFlag || columnFlag || diagonalFlag;
   };
 
-  return { playTurn, checkWinPositions };
+  return { playGame };
 })();
 
-const playerOne = createPlayer("Player 1", "X");
-const playerTwo = createPlayer("Player 2", "O");
+const playerOne = createPlayer("Player 1", "X", false);
+const playerTwo = createPlayer("Player 2", "O", false);
 
+Control.playGame();
 // console.log(Control.checkWinPositions("X"));
 // Control.playTurn();
 // Control.playTurn();
